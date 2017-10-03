5e730c1a742abadc0038c58b31a3eae6ab06bb53
liangwei93 <liangwei93@live.com>
2017-10-03 18:23:10 +0800

    Exploratory Data Analysis is used for doing up preliminary report

--- Exploratory Data Analysis.R
+++ Exploratory Data Analysis.R
@@ -1,3 +1,6 @@
+library(GGally)
+library(DMwR)
+
 ## loading of data
 data <- diabetes
 str(data)
@@ -15,6 +18,7 @@ biological_data
 ## it applies over rows, whereas with MARGIN=2, it works over columns. (in this case we working columns)
 features_miss_num <- apply(biological_data, 2, function(x) sum(x<=0))
 features_miss <- names(biological_data)[ features_miss_num > 0]
+## Show the number of missing values of each column
 features_miss_num
 
 ## LY: 
@@ -30,6 +34,68 @@ sum(rowswitherrors)/nrow(data)
 
 ## those with problems change to NAs
 biological_data[biological_data<=0] <- NA
-dat[, names(biological_data)] <- biological_data
+data[, names(biological_data)] <- biological_data
+
+## saving a copy of all the changes up to now so we can refer back if need be
+original_data <- data
+
+## data visualisation of missing data, as we can see close to 50% of insulin data 
+## and 30% of skinthickness data are missing
+aggr_plot <- aggr(data, col=c('navyblue','red'), numbers=TRUE, 
+                  sortVars=TRUE, labels=names(data), cex.axis=.7, 
+                  gap=3, ylab=c("Histogram of missing data","Pattern"))
+dev.off()
+
+## Exploratory Analysis 
+df <- data.frame(data)
+df <- knnImputation(df, k=5, meth="weighAvg" )
+summary(df)
+## for visualising missing data
+md.pattern(df)
+
+## proportion of the outcome output
+prop.table(table(df$Outcome))
+
+##correlation between numerical variables
+correlat <- cor(df[, setdiff(names(df), 'Outcome')])
+corrplot(correlat)
+correlat
+boxplot(df)
+## the wide range above shows the need to normalise the data
+## below functions are for scaling and checking
+scaled.df <- scale(df[,1:8])
+colMeans(scaled.df)  
+boxplot(scaled.df)
+
+## logistic regression as baseline model for reference to compare 
+## with other techniques
+set.seed(4510)
+dindex <- createDataPartition(df$Outcome, p=0.7, list=FALSE)
+train_data <- df[dindex,]
+test_data <- df[-dindex,]
+table(train_data$Outcome)
+## codes for logistic regression
+fitControl <- trainControl(method = "cv",
+                           number = 10,
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary)
+
+model_glm <- train(Outcome~.,
+                   train_data,
+                   method="glm",
+                   metric="ROC",
+                   tuneLength=10,
+                   preProcess = c('center', 'scale'),
+                   trControl=fitControl)
+
+pred_glm <- predict(model_glm, test_data)
+cm_glm <- confusionMatrix(pred_glm, test_data$Outcome, positive="X1")
+cm_glm
+
+library(pROC)
+library(caTools)
+pred_prob_glm <- predict(model_glm, test_data, type="prob")
+roc_glm <- roc(test_data$Outcome, pred_prob_glm$X1)
+colAUC(pred_prob_glm$X1, test_data$Outcome, plotROC = TRUE)
 
 
--- diabetes_prelimnary explorationLY.R
+++ diabetes_prelimnary explorationLY.R
@@ -1,8 +1,22 @@
 # LY: I'm using the in built R data set of Pima Indians Diabetes so diabetes(outcome) is alr of type factor #
-# testing 123 
+library(mlbench)
+library(VIM)
+library(dplyr)
+library(caret)
+library(ggplot2)
+library(glmnet)
+library(rpart)
+library(partykit)
+library(randomForest)
+library(DMwR)
+library(corrplot)
+library(mice)
+
+
 dat <- PimaIndiansDiabetes
 ## Checking classes of various columns in dataset
 str(dat)
+attach(dat)
 ## Changing the class of Outcome Column into factor
 dat$Outcome <- factor(make.names(dat$Outcome))
 str(dat)
@@ -10,11 +24,15 @@ str(dat)
 summary(dat)
 ## some biological measurements have value 0 in the dataset, and that’s impossible
 ## setdiff function is used to eliminate such problem
-biological_data <- dat[,setdiff(names(dat), c('Outcome', 'Pregnancies'))]
-features_miss_num <- apply(biological_data, 2, function(x) sum(x<=0))
-features_miss <- names(biological_data)[ features_miss_num > 0]
-features_miss_num
-## LW : i got no fucking idea whata are the above doing
+biological_data1 <- dat[,setdiff(names(dat), c('Outcome', 'Pregnancies'))]
+biological_data1
+## these are the arguments for apply(X, MARGIN, FUN, ...)
+## MARGIN is a variable defining how the function is applied: when MARGIN=1, 
+## it applies over rows, whereas with MARGIN=2, it works over columns. (in this case we working columns)
+features_miss_num1 <- apply(biological_data1, 2, function(x) sum(x<=0))
+features_miss1 <- names(biological_data1)[ features_miss_num1 > 0]
+## Show the number of missing values of each column
+features_miss_num1
 
 ## LY: 
 ## Based on wiki, the essential body fat percentage for females (body fat accounts for the skinfold thickness) is 10-13%
@@ -23,21 +41,31 @@ features_miss_num
 ## SO basically the Kaggle code above just checking which feature attribute indeed contains impossible entries of 0 then set them to NAs
 
 
-rows_errors <- apply(biological_data, 1, function(x) sum(x<=0)>1) 
+rows_errors1 <- apply(biological_data1, 1, function(x) sum(x<=0)>1) 
 sum(rows_errors)
 ## These are a lot of rows. It is more than 30% of the dataset:
 sum(rows_errors)/nrow(dat)
 ## those with problems change to NAs
 
 biological_data[biological_data<=0] <- NA
-dat[, names(biological_data)] <- biological_data
+data[, names(biological_data)] <- biological_data
+## saving a copy of all the changes up to now so we can refer back if need be
+original_data <- data
+
+## data visualisation of missing data, as we can see close to 50% of insulin data 
+## and 30% of skinthickness data are missing
+aggr_plot <- aggr(data, col=c('navyblue','red'), numbers=TRUE, 
+                  sortVars=TRUE, labels=names(data), cex.axis=.7, 
+                  gap=3, ylab=c("Histogram of missing data","Pattern"))
+dev.off()
+marginplot(data[c(1,2)])
 
 ## Then to deal with the NAs, instead of using the usual method of setting NAs to 0 (which doesnt work for our dataset)
 ## Use knnImputation (i.e. KNN method to input N.As with the k-nearest-neighbouring value)
-library(VIM)
-dat_original <- dat
+
 dat[,-9] <- knnImputation(dat[,-9], k=5)
 ## got stuck  #LY: works fine for me# 
+md.pattern(data)
 
 ## proportion of the outcome output
 prop.table(table(dat$Outcome))
